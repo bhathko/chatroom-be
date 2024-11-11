840cd8824dcb637882b73cf16aa258dfe2b4e351
Hunter Ko <hunter@Hunters-MBP.localdomain>
2024-11-11 13:59:50 +0800

    fix(message): miss import module for typeorm

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { CoreModule } from '../core/core.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatRoom } from '../chat-room/entities/chat-room.entity';
+import { User } from '../users/entities/user.entity';
+import { Message } from './entities/message.entity';
 
 @Module({
   controllers: [MessagesController],
   providers: [MessagesService],
-  imports: [CoreModule],
+  imports: [CoreModule,
+    TypeOrmModule.forFeature([Message, User, ChatRoom])
+  ],
 })
 export class MessagesModule {}
